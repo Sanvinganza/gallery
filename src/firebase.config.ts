6da47ca4b50e78b7ca0194a3dabe35f8c65dff63
Sanvinganza <anton.shvetsov@mail.ru>
2023-11-01 16:01:40 +0300

    init project create button component

---
+++ src/firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAabobHROqDOM8ii5NhkynebIAcvAjl_ao",
+  authDomain: "gallery-48b36.firebaseapp.com",
+  projectId: "gallery-48b36",
+  storageBucket: "gallery-48b36.appspot.com",
+  messagingSenderId: "376692135080",
+  appId: "1:376692135080:web:c92acb2b04948647765e9d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
