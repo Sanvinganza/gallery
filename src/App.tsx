7acb16007c136ba6c2741c7af10fba0a10836fb1
Sanvinganza <anton.shvetsov@mail.ru>
2023-11-09 01:23:31 +0300

    fised: delete useless imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { SignUpForm } from "./components/SignupForm/SignupForm";
 import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
-import { HomePage } from "./pages/HomePage";
 import { GalleryPage } from "./pages/GalleryPage/GalleryPage";
 
 function App() {
